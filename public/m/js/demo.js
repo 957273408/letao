31ef5d5ebb4fe2f2d8d8352de578ca3e3085ef01
957273408 <957273408@qq.com>
2019-01-05 15:13:40 +0800

    2019/1/5

---
+++ public/m/js/demo.js
@@ -0,0 +1,3 @@
+let arr=[5,6,7,8,9];
+arr.splice(0);
+console.log(arr)
--- public/m/js/index.js
+++ public/m/js/index.js
@@ -1,16 +1,10 @@
-$(function() {
-
-    //初始化轮播图
-    // mui('.mui-slider').slider({
-    //     interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
-    // });
-    var slider = mui("#slider");
-    slider.slider({
-        interval: 5000
-    });
-
-    // 初始化区域滚动
-    mui('.mui-scroll-wrapper').scroll({
-        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    });
-})
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:500//自动轮播周期，若为0则不自动播放，默认为0；
+});
+mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false,
+    bounce:true,
+});
